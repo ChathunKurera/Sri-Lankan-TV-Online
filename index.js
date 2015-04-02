21db08fa9efd38886290092b427f150274d51f61
vishvvaw <vizzminzi@gmail.com>
2015-04-01 20:54:41 -0500

    added index js

---
+++ index.js
@@ -0,0 +1,19 @@
+function myFunction(video_id) {
+document.getElementById("video").src = "http://www.ustream.tv/embed/"+ video_id +"?v=3&amp;wmode=direct";
+}
+function myFunctio(video_id2) {
+switch (video_id2) {
+case 1324: video_id2 ="WO8WdcH72Js" ; break;
+}
+document.getElementById("video").src = "https://www.youtube.com/embed/"+ video_id2 +"?rel=0&amp;controls=0&amp;showinfo=0";
+}
+function myFunction2(video_id3) {
+switch (video_id3) {
+case 1: video_id3 ="HiruTV.html" ; break;
+case 2: video_id3 ="Rupavahini.html" ; break;
+case 3: video_id3 ="ITN.html" ; break;
+case 4: video_id3 = "Derana.html"; break;
+case 6: video_id3 ="NewsFirst.html " ; break;
+}
+document.getElementById("video").src = "http://vishvvaw.github.io/SriLankanTVOnline/" + video_id3;
+} 
