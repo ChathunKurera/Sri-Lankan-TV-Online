24ba097621019252eb8d3dbc13a343731e7df8cf
vishwa <vizzminzi@gmail.com>
2015-04-01 21:34:28 -0500

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -4,7 +4,17 @@ var url = document.location.href;
  
 if(window.sidebar){
 /* Mozilla Firefox Bookmark */
-window.sidebar.addPanel(title, url, "");
+$('#bookmarkme').click(function() {
+            if (window.sidebar && window.sidebar.addPanel) { // Mozilla Firefox Bookmark
+                window.sidebar.addPanel(document.title,window.location.href,'');
+            } else if(window.external && ('AddFavorite' in window.external)) { // IE Favorite
+                window.external.AddFavorite(location.href,document.title); 
+            } else if(window.opera && window.print) { // Opera Hotlist
+                this.title=document.title;
+                return true;
+            } else { // webkit - safari/chrome
+                alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != - 1 ? 'Command/Cmd' : 'CTRL') + ' + D to bookmark this page.');
+            }
 }else if(window.external){
 /* IE Favorite */
 window.external.AddFavorite(url, title);
