8bc21d4726bccdaacf94749d80de70b5323248d7
vishwa <vizzminzi@gmail.com>
2015-04-01 22:24:10 -0500

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,12 +1,19 @@
-window.external.AddFavorite(location.href, document.title);
-$(document).ready(function() {
-    $("#bookmarkme").click(function() {
-      if (window.sidebar) { // Mozilla Firefox Bookmark
-        window.sidebar.addPanel(location.href,document.title,"");
-      } else if(window.external) { // IE Favorite
-        window.external.AddFavorite(location.href,document.title); }
-      else if(window.opera && window.print) { // Opera Hotlist
-        this.title=document.title;
-        return true;
-  }
+function CreateBookmarkLink(){
+var title = document.title;
+var url = document.location.href;
+window.external.AddFavorite(url, title);}
+$(function() {
+$('#bookmarkme').click(function() {
+if (window.sidebar && window.sidebar.addPanel) { // Mozilla Firefox Bookmark
+window.sidebar.addPanel(document.title,window.location.href,'');
+} else if(window.external && ('AddFavorite' in window.external)) { // IE Favorite
+window.external.AddFavorite(location.href,document.title);
+} else if(window.opera && window.print) { // Opera Hotlist
+this.title=document.title;
+return true;
+} else { // webkit - safari/chrome
+alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != - 1 ? 'Command/Cmd' : 'CTRL') + ' + D to bookmark this page.');
+}
 });
+});
+javascript:(function(){var d=document,e=d.getElementById("someElement");e.value="some value";})();
