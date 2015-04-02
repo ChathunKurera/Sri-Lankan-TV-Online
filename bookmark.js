14e608a544a0018cb1b1d031bb791d0f10c2476e
vishwa <vizzminzi@gmail.com>
2015-04-01 22:20:21 -0500

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -3,19 +3,20 @@ var title = document.title;
 var url = document.location.href;
 var ua = navigator.userAgent;
 
-var elem = document.createElement('a');
-        elem.setAttribute('href',url);
-        elem.setAttribute('title',title);
-        elem.setAttribute('rel','sidebar');
-        elem.click(); // this.title=document.title;
 
-$(document).ready(function() {
-    $("#bookmarkme").click(function() {
-      if (window.sidebar) { // Mozilla Firefox Bookmark
-        window.sidebar.addPanel(location.href,document.title,"");
-      } else if(window.external) { // IE Favorite
-        window.external.AddFavorite(location.href,document.title); }
-      else if(window.opera && window.print) { // Opera Hotlist
-        this.title=document.title;
-        return true;
-  }
+window.external.AddFavorite(url, title);}
+$(function() {
+$('#bookmarkme').click(function() {
+if (window.sidebar && window.sidebar.addPanel) { // Mozilla Firefox Bookmark
+window.sidebar.addPanel(document.title,window.location.href,'');
+} else if(window.external && ('AddFavorite' in window.external)) { // IE Favorite
+window.external.AddFavorite(location.href,document.title);
+} else if(window.opera && window.print) { // Opera Hotlist
+this.title=document.title;
+return true;
+} else { // webkit - safari/chrome
+alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != - 1 ? 'Command/Cmd' : 'CTRL') + ' + D to bookmark this page.');
+}
+});
+});
+}
