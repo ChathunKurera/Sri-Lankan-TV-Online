61036d7400a1d747258714b13765c85223de0ea6
vishwa <vizzminzi@gmail.com>
2015-04-01 21:32:16 -0500

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,13 +1,19 @@
-function bookmarksite(title,url){
-if (window.sidebar) // firefox
-	window.sidebar.addPanel(title, url, "");
-else if(window.opera && window.print){ // opera
-	var elem = document.createElement('a');
-	elem.setAttribute('href',url);
-	elem.setAttribute('title',title);
-	elem.setAttribute('rel','sidebar');
-	elem.click();
-} 
-else if(document.all)// ie
-	window.external.AddFavorite(url, title);
+function CreateBookmarkLink(){
+var title = document.title;
+var url = document.location.href;
+ 
+if(window.sidebar){
+/* Mozilla Firefox Bookmark */
+window.sidebar.addPanel(title, url, "");
+}else if(window.external){
+/* IE Favorite */
+window.external.AddFavorite(url, title);
+}else if(window.opera && window.print) {
+/* Opera Hotlist */
+alert("Press Control + D to bookmark");
+return true;
+}else{
+/* Other */
+alert("Press Control + D to bookmark");
+}
 }
