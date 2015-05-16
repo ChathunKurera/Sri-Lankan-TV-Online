53cc58c8be284457d739f4b5b311931ff06075c2
vishvvaw <vizzminzi@gmail.com>
2015-05-16 14:18:45 -0500

    New News First Layout

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ document.getElementById("video").src = "http://www.ustream.tv/embed/"+ video_id 
 }
 function myFunctio(video_id2) {
 switch (video_id2) {
-case 1324: video_id2 ="2h2liR3goX0" ; break;
+case 2h2liR3goX0:document.getElementById("video").src = "https://www.youtube.com/embed/"+ video_id2 +"?autoplay=1"; break;
+case 44545: document.getElementById("video").src = "www.youtube.com/embed?listType=user_uploads&list=NewsFirstSrilanka"; break;
 }
-document.getElementById("video").src = "https://www.youtube.com/embed/"+ video_id2 +"?autoplay=1";
+
 }
 function myFunction2(video_id3) {
 switch (video_id3) {
@@ -13,7 +14,6 @@ case 1: video_id3 ="Hiru.html" ; break;
 case 2: video_id3 ="Rupavahini.html" ; break;
 case 3: video_id3 ="Itn.html" ; break;
 case 4: video_id3 = "Derana.html"; break;
-case 6: video_id3 ="NewsFirst.html " ; break;
 }
 document.getElementById("video").src = "http://vishvvaw.github.io/Sri-Lankan-TV-Online/" + video_id3;
 } 
