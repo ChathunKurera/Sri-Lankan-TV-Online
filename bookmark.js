64c758f3d32a06f25f37e8fdad063438ce78778f
vishwa <vizzminzi@gmail.com>
2015-06-21 15:34:56 -0500

    Removing unwanted useless code

--- bookmark.js
+++ bookmark.js
@@ -1,16 +1,3 @@
-//  $(document).ready(function() {
-    //$("#bookmarkme").click(function() {
-      //if (window.sidebar) { // Mozilla Firefox Bookmark
-        //window.sidebar.addPanel(location.href,document.title,"");
-      //} else if(window.external) { // IE Favorite
-      //  window.external.AddFavorite(location.href,document.title); }
-      //  alert("CTRL + D too bookmark this website!")
-     // else if(window.opera && window.print) { // Opera Hotlist
-    //    this.title=document.title;
-  //       alert("CTRL + D too bookmark this website!");
-//        return true;
-//  }
-//});
 var ios = Components.classes["@mozilla.org/network/io-service;1"]
                     .getService(Components.interfaces.nsIIOService);
 var uri = ios.newURI("http://google.com/", null, null);
