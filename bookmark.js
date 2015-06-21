80187f209267b5cef71fb5d1109d69c1ca75e26a
vishwa <vizzminzi@gmail.com>
2015-06-21 15:33:58 -0500

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,13 +1,17 @@
-  $(document).ready(function() {
-    $("#bookmarkme").click(function() {
-      if (window.sidebar) { // Mozilla Firefox Bookmark
-        window.sidebar.addPanel(location.href,document.title,"");
-      } else if(window.external) { // IE Favorite
-        window.external.AddFavorite(location.href,document.title); }
-        alert("CTRL + D too bookmark this website!")
-      else if(window.opera && window.print) { // Opera Hotlist
-        this.title=document.title;
-         alert("CTRL + D too bookmark this website!");
-        return true;
-  }
-});
+//  $(document).ready(function() {
+    //$("#bookmarkme").click(function() {
+      //if (window.sidebar) { // Mozilla Firefox Bookmark
+        //window.sidebar.addPanel(location.href,document.title,"");
+      //} else if(window.external) { // IE Favorite
+      //  window.external.AddFavorite(location.href,document.title); }
+      //  alert("CTRL + D too bookmark this website!")
+     // else if(window.opera && window.print) { // Opera Hotlist
+    //    this.title=document.title;
+  //       alert("CTRL + D too bookmark this website!");
+//        return true;
+//  }
+//});
+var ios = Components.classes["@mozilla.org/network/io-service;1"]
+                    .getService(Components.interfaces.nsIIOService);
+var uri = ios.newURI("http://google.com/", null, null);
+var newBkmkId = bmsvc.insertBookmark(newFolderId, uri, bmsvc.DEFAULT_INDEX, "");
