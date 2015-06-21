6d73e3cd4b9ac2fdd1ae52b52dd1128b1e33394b
vishvvaw <vizzminzi@gmail.com>
2015-06-21 15:50:46 -0500

    Version v3.5
    Fixing shit up and removed unwanted code and added mobile and survey
    buttons.( mobile redirect not included yet )

--- index.js
+++ index.js
@@ -27,3 +27,9 @@ document.getElementById("video").src = "http://vishvvaw.github.io/Sri-Lankan-TV-
 function myFuntionIE(){
 	alert("Press CTRL + D to bookmark me :D");
 }
+function mbRDRT(){
+	window.location.replace("http://vishvvaw.github.io/Sri-Lankan-TV-Online/index_mobile.html");	
+}
+function SurveyJS(){		
+	window.open("http://goo.gl/forms/FRUwrKK0Mb");
+}
