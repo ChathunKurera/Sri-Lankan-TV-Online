d564670beba43d341575ef8afca007db368427e3
vishvvaw <vizzminzi@gmail.com>
2015-07-24 20:43:20 -0500

    Video Link update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById("video").src = "http://www.ustream.tv/embed/"+ video_id 
 }
 function youtubeStream(video_id2) {
 switch (video_id2) {
-case 22222: video_id2 ="t19heQM2YmA";break;
+case 22222: video_id2 ="yZWhtPEjKbA";break;
 }
 document.getElementById("video").src = "https://www.youtube.com/embed/"+ video_id2 +"?theme=light&autoplay=1";
 }
