444e7cc976babf85918741bbf017622076e6272a
vishvvaw <vizzminzi@gmail.com>
2016-01-06 11:02:24 -0600

    Video Link Update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ switch (video_id2) {
 case 22222: video_id2 ="yZWhtPEjKbA";break;
 case 33333: video_id2 ="cqsgq8o2JLk";break;
 case 44444: video_id2 ="foLxon7B96Y";break;
+case 55555: video_id2 ="_B3R4j_zYuE";break;
 }
 document.getElementById("video").src = "https://www.youtube.com/embed/"+ video_id2 +"?theme=light&autoplay=1";
 }
